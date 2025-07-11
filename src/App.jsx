cf0d821f0c56df30ae22b56950d4fb16e4fc5ff6
DawoodIfzaal <dawoodifzaalpr@gmail.com>
2025-07-11 15:29:38 +0530

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>This is a blog {import.meta.env.VITE_VARIABLE}</h1>
+      <h1>This is a blog</h1>
     </>
   )
 }
